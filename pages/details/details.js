49d24f6b7ad16ac4fb58a39cdbfd164f019f3f35
chenchen <chenchen@getwherewolf.com>
2019-04-28 12:00:37 +1200

    Failed attempt to display content properly (conbain text and image)

--- pages/details/details.js
+++ pages/details/details.js
@@ -1,66 +1,51 @@
 // pages/details/details.js
+//新闻详情页
+
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-
+    id: '1552623252490', //调试模式
+    //调试结束以后恢复
+    //id: '',
+    details: {},
   },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
 
+  onLoad(options) {
+    //调试结束以后恢复
+    /*this.setData({
+      id: options.id
+    })*/
+    this.getDetail()
   },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
+  //获取新闻详情
+  getDetail(callback) {
+      wx.request({
+        url: 'https://test-miniprogram.com/api/news/detail',
+        data: {
+          'id': this.data.id
+        },
+        success: res => {
+          let details = res.data.result
+          details.date = details.date.slice(0,10)
+          this.setData({
+            details: details
+          })
+          this.detailsContent(details.content)
+        },
+        complete: () => {
+          callback && callback()
+        }
+      })
+    },
 
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  //判断新闻详情内容（content）是图片还是文字
+  detailsContent(content) {
+    console.log(content)
+    let contentFormat = []
+    for (let i = 0; i < content.length; i++) {
+      contentFormat[i] = content[i].type === 'image' ? "<image class='content-image' src='{{content[i].firstImage}}' mode='aspectFill'></image>" : "<view class='content-para'>{{content[i].text}}</view>"
+    }
+    console.log(contentFormat)
   }
 })
